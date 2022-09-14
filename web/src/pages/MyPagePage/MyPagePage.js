b30aa15ac344cdd5c03a19f2074f7c6e1e928174
QaziTalha4595 <97889460+QaziTalha4595@users.noreply.github.com>
2022-09-14 16:39:14 +0530

    modified:   web/src/pages/MyPagePage/MyPagePage.js

--- web/src/pages/MyPagePage/MyPagePage.js
+++ web/src/pages/MyPagePage/MyPagePage.js
@@ -1,12 +1,13 @@
 import { Link, routes } from '@redwoodjs/router'
 import { MetaTags } from '@redwoodjs/web'
+import UsersesCell from 'src/components/Users/UsersesCell'
 
 const MyPagePage = () => {
   return (
     <>
       <MetaTags title="MyPage" description="MyPage page" />
 
-sdjkwebfkjwe
+    <><UsersesCell /></>
     </>
   )
 }
