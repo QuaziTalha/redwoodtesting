fb68d9d41a81fe065e9d7230ad78fdd6343fa246
QaziTalha4595 <97889460+QaziTalha4595@users.noreply.github.com>
2022-09-14 13:24:28 +0530

    First commit

---
+++ web/src/pages/MyPagePage/MyPagePage.js
@@ -0,0 +1,15 @@
+import { Link, routes } from '@redwoodjs/router'
+import { MetaTags } from '@redwoodjs/web'
+
+const MyPagePage = () => {
+  return (
+    <>
+      <MetaTags title="MyPage" description="MyPage page" />
+
+      <h1>Hello There</h1>
+    <p>ont minnd me just testing</p>
+    </>
+  )
+}
+
+export default MyPagePage
